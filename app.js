f0e972173277c6cd1634b2c98bd41f5f16166d89
Xujuan <767196201@qq.com>
2019-11-22 10:58:11 +0800

    feat: 单页面模板

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
 
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
       url: req.url
     },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
+    template: `<div>the url is： {{ url }}</div>`
   })
 
-  renderer.renderToString(app, (err, html) => {
+  const context = {
+    title: 'hello',
+    meta: `
+    <meta charset="utf-8">
+    `
+  }
+
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    console.log(html)
+    res.end(html)
   })
 })
 
